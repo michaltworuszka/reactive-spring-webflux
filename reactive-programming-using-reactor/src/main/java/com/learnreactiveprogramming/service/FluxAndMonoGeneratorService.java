398cbc02d1456d9cdc3ebf586dd6850f030ebac8
michaltworuszka <michal.tworuszka@gmail.com>
2023-03-22 13:49:22 +0100

    Base Flux/Mono and testing

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,28 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux(){
+
+        return Flux.fromIterable(List.of("mike", "mykola", "kris"))
+                .log(); //creates a Flux from collection like List
+    }
+
+    public Mono<String> nameMono(){
+        return Mono.just("sandeep").log();
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name ->
+                        System.out.println("Name is: " + name));
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe(name -> System.out.println("Mono name is: " + name));
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,30 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService =
+            new FluxAndMonoGeneratorService();
+
+
+    @Test
+    void namesFlux() {
+        //given
+
+        //when
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("mike", "mykola", "kris")
+                .expectNextCount(0)//how many you expect after previous method is called
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
 }
