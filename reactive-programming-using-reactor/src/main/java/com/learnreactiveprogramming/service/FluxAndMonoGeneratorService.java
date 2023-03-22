eddbffd0ec71b661175e243fdad3b8f51d56c4e1
michaltworuszka <michal.tworuszka@gmail.com>
2023-03-22 14:48:18 +0100

    Operators

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -7,14 +7,47 @@ import java.util.List;
 
 public class FluxAndMonoGeneratorService {
 
-    public Flux<String> namesFlux(){
+    public Flux<String> namesFlux() {
 
-        return Flux.fromIterable(List.of("mike", "mykola", "kris"))
-                .log(); //creates a Flux from collection like List
+        return Flux.fromIterable(List.of("mikey", "mykola", "kris")) //creates a Flux from collection like List
+                .log();
     }
 
-    public Mono<String> nameMono(){
+    public Mono<String> nameMono() {
         return Mono.just("sandeep").log();
+    }
+
+    public Flux<String> namesFlux_map(int stringLength) {
+
+        return Flux.fromIterable(List.of("mikey", "mykola", "kris"))
+                //.map(name -> name.toUpperCase());
+                .map(String::toUpperCase)
+                .filter(string -> string.length() > stringLength)
+                .map(s -> s.length() + "-" + s)
+                .log();
+    }
+
+    public Flux<String> namesFlux_immutability() {
+
+        Flux<String> namesFlux = Flux.fromIterable(List.of("mikey", "mykola", "kris"));
+        namesFlux.map(String::toUpperCase);
+        return namesFlux.log(); //this will return lowerCase - because we map this flux but we didn't introduce the new mapped flux to any variable,
+        // so we are returning the unchanged one
+    }
+
+    public Flux<String> namesFlux_flatMap(int stringLength) {
+
+        return Flux.fromIterable(List.of("mikey", "mykola", "kris"))
+                .map(String::toUpperCase)
+                .filter(string -> string.length() > stringLength)
+                //MIKEY, MYKOLA -> M,I,K,E,Y,M,Y,K,O,L,A
+                .flatMap(this::splitString)
+                .log();
+    }
+
+    public Flux<String> splitString(String name){
+        var charArray = name.split("");
+        return Flux.fromArray(charArray);
     }
 
     public static void main(String[] args) {
@@ -24,5 +57,9 @@ public class FluxAndMonoGeneratorService {
                         System.out.println("Name is: " + name));
         fluxAndMonoGeneratorService.nameMono()
                 .subscribe(name -> System.out.println("Mono name is: " + name));
+
+        fluxAndMonoGeneratorService.namesFlux_map(2)
+                .subscribe(name ->
+                        System.out.println("Mapped name is: " + name));
     }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -19,12 +19,57 @@ class FluxAndMonoGeneratorServiceTest {
 
         //then
         StepVerifier.create(namesFlux)
-                .expectNext("mike", "mykola", "kris")
+                .expectNext("mikey", "mykola", "kris")
                 .expectNextCount(0)//how many you expect after previous method is called
                 .verifyComplete();
 
         StepVerifier.create(namesFlux)
                 .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_map() {
+        //given
+        int stringLength = 4;
+
+        //when
+        Flux<String> namesFlux_mapped = fluxAndMonoGeneratorService.namesFlux_map(stringLength);
+
+        //then
+        StepVerifier.create(namesFlux_mapped)
+                .expectNext("mikey".length() + "-" + "mikey".toUpperCase() ,"mykola".length() + "-" + "mykola".toUpperCase())
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_immutability() {
+        //given
+
+        //when
+        Flux<String> namesFlux_immutability = fluxAndMonoGeneratorService.namesFlux_immutability();
+
+        //then
+        StepVerifier.create(namesFlux_immutability)
+                .expectNext("mikey", "mykola", "kris")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_flatMap() {
+        //given
+        int stringLength = 4;
+
+        //when
+        Flux<String> namesFlux_flatMapped = fluxAndMonoGeneratorService.namesFlux_flatMap(stringLength);
+
+        //then
+        StepVerifier.create(namesFlux_flatMapped)
+                .expectNextCount(11)
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux_flatMapped)
+                .expectNext("M","I","K","E","Y","M","Y","K","O","L","A")
                 .verifyComplete();
     }
 }
